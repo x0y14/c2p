eda2cf78e4f89845d1af930309711b27e1700e2d
x0y14 <ya01ma08@gmail.com>
2023-11-08 00:00:42 +0900

    やりかけ

--- python/Gen/gen.go
+++ python/Gen/gen.go
@@ -3,86 +3,178 @@ package Gen
 import (
 	"c2p/python/parse"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
+var nest int
+var code string
+
+func init() {
+	nest = 0
+	code = ""
+}
+
+func indent() string {
+	return strings.Repeat("\t", nest)
+}
+
+func list(s string) []string {
+	return []string{s}
+}
+
 func Gen(nodes []*parse.Node) (string, error) {
-	code, err := program(nodes)
+	err := program(nodes)
 	if err != nil {
 		return "", err
 	}
+	code += "if __name__ == \"__main__\":\n\tmain()"
 	return code, nil
 }
 
-func program(nodes []*parse.Node) (string, error) {
-	code := ""
+func program(nodes []*parse.Node) error {
 	for _, node := range nodes {
-		tl, err := toplevel(node)
+		err := toplevel(node)
 		if err != nil {
-			return "", err
+			return err
 		}
-		code += tl + "\n"
 	}
-	return code, nil
+	return nil
 }
 
-func toplevel(node *parse.Node) (string, error) {
+func toplevel(node *parse.Node) error {
 	switch node.Kind {
 	case parse.FunctionDefine:
 		return functionDefine(node)
 	}
-	return "", nil
+	return nil
 }
 
-func functionDefine(node *parse.Node) (string, error) {
+func functionDefine(node *parse.Node) error {
 	field := node.FunctionDefineField
 
-	fnName := field.Ident.S
-	fnParams, err := funcDefineParameters(field.Params)
+	ident := field.Ident.S
+
+	params, err := functionDefineParams(field.Params)
 	if err != nil {
-		return "", err
+		return err
 	}
-	statements, err := stmt(field.Block)
+
+	bl, err := stmt(field.Block)
 	if err != nil {
-		return "", err
+		return err
 	}
-	code := fnName + fnParams + statements
-	return code, nil
+
+	c := fmt.Sprintf("def %s(%s):", ident, params) + "\n"
+	c += bl[0]
+	c += "\n"
+
+	code += c
+
+	return nil
 }
 
-func funcDefineParameters(node *parse.Node) (string, error) {
-	_ = node
+func functionDefineParams(node *parse.Node) (string, error) {
 	return "", nil
 }
 
-func stmt(node *parse.Node) (string, error) {
+func stmt(node *parse.Node) ([]string, error) {
 	switch node.Kind {
 	case parse.Block:
-		var code string
-		for _, statement := range node.BlockField.Stmts {
-			c, err := stmt(statement)
+		var c string
+		nest++
+		for _, statementNode := range node.BlockField.Stmts {
+			statements, err := stmt(statementNode)
 			if err != nil {
-				return "", err
+				return nil, err
 			}
-			code += c
+			for _, line := range statements {
+				c += fmt.Sprintf("%s%s", indent(), line) + "\n"
+			}
 		}
-		return code, nil
+		nest--
+		return list(c), nil
 	case parse.Return:
-		rv := ""
-		for _, v := range node.ReturnField.Values {
-			rv_, err := expr(v)
+		var values []string
+		for _, valueNode := range node.ReturnField.Values {
+			v, err := expr(valueNode)
 			if err != nil {
-				return "", err
+				if err != nil {
+					return nil, err
+				}
 			}
-			rv += rv_
+			values = append(values, v)
 		}
-		return "return " + rv, nil
-
+		return list(fmt.Sprintf("return %s", strings.Join(values, ", "))), nil
 	case parse.IfElse:
-	case parse.While:
-	case parse.For:
+		field := node.IfElseField
+		cond, err := expr(field.Cond)
+		if err != nil {
+			return nil, err
+		}
+		ifBlock, err := stmt(field.IfBlock)
+		if err != nil {
+			return nil, err
+		}
 
+		if field.ElseBlock == nil {
+			// ifだけ
+			rt := []string{
+				fmt.Sprintf("if (%s):", cond),
+			}
+			rt = append(rt, ifBlock...)
+			return rt, nil
+		}
+
+		elseBlock, err := stmt(field.ElseBlock)
+		if err != nil {
+			return nil, err
+		}
+
+		rt := []string{
+			fmt.Sprintf("if (%s):", cond),
+		}
+		rt = append(rt, ifBlock...)
+		rt = append(rt, []string{
+			"else:",
+		}...)
+		rt = append(rt, elseBlock...)
+		return rt, nil
+	case parse.While:
+		field := node.WhileField
+		wInit, err := expr(field.Init)
+		if err != nil {
+			return nil, err
+		}
+		wCond, err := expr(field.Cond)
+		if err != nil {
+			return nil, err
+		}
+		wLoop, err := expr(field.Loop)
+		if err != nil {
+			return nil, err
+		}
+		wBlock, err := stmt(field.Block)
+		if err != nil {
+			return nil, err
+		}
+		// blockの最後にloopを入れてあげる
+		wBlock = append(wBlock, wLoop)
+
+		rt := []string{
+			wInit,
+			fmt.Sprintf("while (%s):", wCond),
+		}
+		rt = append(rt, wBlock...)
+		return rt, nil
+	case parse.For:
 	}
-	return expr(node)
+
+	e, err := expr(node)
+	if err != nil {
+		return nil, err
+	}
+	return list(e), nil
 }
 
 func expr(node *parse.Node) (string, error) {
@@ -102,27 +194,166 @@ func assign(node *parse.Node) (string, error) {
 		}
 		return fmt.Sprintf("%s = %s", to, val), nil
 	}
-
 	return andor(node)
 }
 
 func andor(node *parse.Node) (string, error) {
+	switch node.Kind {
+	case parse.And:
+		lhs, err := andor(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := andor(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s and %s", lhs, rhs), nil
+	case parse.Or:
+		lhs, err := andor(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := andor(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s or %s", lhs, rhs), nil
+	}
 	return equality(node)
 }
 
 func equality(node *parse.Node) (string, error) {
+	switch node.Kind {
+	case parse.Eq:
+		lhs, err := equality(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := equality(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s == %s", lhs, rhs), nil
+	case parse.Ne:
+		lhs, err := equality(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := equality(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s != %s", lhs, rhs), nil
+	}
 	return relational(node)
 }
 
 func relational(node *parse.Node) (string, error) {
+	switch node.Kind {
+	case parse.Lt:
+		lhs, err := relational(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := relational(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s < %s", lhs, rhs), nil
+	case parse.Le:
+		lhs, err := relational(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := relational(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s <= %s", lhs, rhs), nil
+	case parse.Gt:
+		lhs, err := relational(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := relational(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s > %s", lhs, rhs), nil
+	case parse.Ge:
+		lhs, err := relational(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := relational(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s >= %s", lhs, rhs), nil
+	}
 	return add(node)
 }
 
 func add(node *parse.Node) (string, error) {
+	switch node.Kind {
+	case parse.Add:
+		lhs, err := add(node.LHS)
+		if err != nil {
+			return "", nil
+		}
+		rhs, err := add(node.RHS)
+		if err != nil {
+			return "", nil
+		}
+		return fmt.Sprintf("%s + %s", lhs, rhs), nil
+	case parse.Sub:
+		lhs, err := add(node.LHS)
+		if err != nil {
+			return "", nil
+		}
+		rhs, err := add(node.RHS)
+		if err != nil {
+			return "", nil
+		}
+		return fmt.Sprintf("%s - %s", lhs, rhs), nil
+	}
 	return mul(node)
 }
 
 func mul(node *parse.Node) (string, error) {
+	switch node.Kind {
+	case parse.Mul:
+		lhs, err := mul(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := mul(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s * %s", lhs, rhs), nil
+	case parse.Div:
+		lhs, err := mul(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := mul(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s / %s", lhs, rhs), nil
+	case parse.Mod:
+		lhs, err := mul(node.LHS)
+		if err != nil {
+			return "", err
+		}
+		rhs, err := mul(node.RHS)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s %% %s", lhs, rhs), nil
+	}
 	return unary(node)
 }
 
@@ -131,9 +362,21 @@ func unary(node *parse.Node) (string, error) {
 }
 
 func primary(node *parse.Node) (string, error) {
+	switch node.Kind {
+	case parse.Ident:
+		return node.S, nil
+	case parse.Call:
+		return call(node)
+	case parse.Int:
+		return strconv.Itoa(node.I), nil
+	case parse.Float:
+		return strconv.FormatFloat(node.F, 'f', -1, 64), nil
+	case parse.String:
+		return node.S, nil
+	}
 	return "", nil
 }
 
 func call(node *parse.Node) (string, error) {
-	return "", nil
+	return "call()", nil
 }
--- python/Gen/gen_test.go
+++ python/Gen/gen_test.go
@@ -2,6 +2,7 @@ package Gen
 
 import (
 	"c2p/python/parse"
+	"fmt"
 	"github.com/google/go-cmp/cmp"
 	"testing"
 )
@@ -41,9 +42,142 @@ func TestGen(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
+			fmt.Println(got)
 			if diff := cmp.Diff(tt.expect, got); diff != "" {
 				t.Errorf("%v", diff)
 			}
 		})
 	}
+}
+
+func TestGen2(t *testing.T) {
+	fizzbuzz := []*parse.Node{
+		parse.NewFunctionDefineFieldNode(
+			parse.NewIdentNode("main"),
+			nil,
+			parse.NewBlockFieldNode([]*parse.Node{
+				parse.NewWhileFieldNode(
+					parse.NewAssignFieldNode( // i = 1
+						parse.NewIdentNode("i"),
+						parse.NewIntNode(1),
+					),
+					parse.NewBinaryFieldNode( // i <= 100
+						parse.Le,
+						parse.NewIdentNode("i"),
+						parse.NewIntNode(100),
+					),
+					parse.NewAssignFieldNode( // i = i+1
+						parse.NewIdentNode("i"),
+						parse.NewBinaryFieldNode(
+							parse.Add,
+							parse.NewIdentNode("i"),
+							parse.NewIntNode(1),
+						),
+					),
+					parse.NewBlockFieldNode([]*parse.Node{
+						parse.NewIfElseFieldNode( // (i % 3 == 0 && i % 5 == 0)
+							parse.NewBinaryFieldNode(
+								parse.And,
+								parse.NewBinaryFieldNode( // i % 3 == 0
+									parse.Eq,
+									parse.NewBinaryFieldNode(
+										parse.Mod,
+										parse.NewIdentNode("i"),
+										parse.NewIntNode(3),
+									),
+									parse.NewIntNode(0),
+								),
+								parse.NewBinaryFieldNode( // i % 5 == 0
+									parse.Eq,
+									parse.NewBinaryFieldNode(
+										parse.Mod,
+										parse.NewIdentNode("i"),
+										parse.NewIntNode(5),
+									),
+									parse.NewIntNode(0),
+								),
+							),
+							parse.NewBlockFieldNode([]*parse.Node{
+								// printf("FizzBuzz\n");
+								parse.NewCallFieldNode(
+									parse.NewIdentNode("printf"),
+									parse.NewPolynomialFieldNode(
+										parse.CallArgs,
+										[]*parse.Node{
+											parse.NewStringNode("FizzBuzz\n"),
+										},
+									),
+								),
+							}),
+							parse.NewIfElseFieldNode( // (i % 3 == 0)
+								parse.NewBinaryFieldNode(
+									parse.Eq,
+									parse.NewBinaryFieldNode(
+										parse.Mod,
+										parse.NewIdentNode("i"),
+										parse.NewIntNode(3),
+									),
+									parse.NewIntNode(0),
+								),
+								parse.NewBlockFieldNode([]*parse.Node{
+									// printf("Fizz\n");
+									parse.NewCallFieldNode(
+										parse.NewIdentNode("printf"),
+										parse.NewPolynomialFieldNode(
+											parse.CallArgs,
+											[]*parse.Node{
+												parse.NewStringNode("Fizz\n"),
+											},
+										),
+									),
+								}),
+								parse.NewIfElseFieldNode( // i % 5 == 0
+									parse.NewBinaryFieldNode(
+										parse.Eq,
+										parse.NewBinaryFieldNode(
+											parse.Mod,
+											parse.NewIdentNode("i"),
+											parse.NewIntNode(5),
+										),
+										parse.NewIntNode(0),
+									),
+									parse.NewBlockFieldNode([]*parse.Node{
+										parse.NewCallFieldNode(
+											parse.NewIdentNode("printf"),
+											parse.NewPolynomialFieldNode(
+												parse.CallArgs,
+												[]*parse.Node{
+													parse.NewStringNode("Buzz\n"),
+												},
+											),
+										),
+									}),
+									parse.NewBlockFieldNode([]*parse.Node{
+										parse.NewCallFieldNode(
+											parse.NewIdentNode("printf"),
+											parse.NewPolynomialFieldNode(
+												parse.CallArgs,
+												[]*parse.Node{
+													parse.NewStringNode("%d\n"),
+													parse.NewIdentNode("i"),
+												},
+											),
+										),
+									}),
+								),
+							),
+						),
+					}),
+				),
+				parse.NewReturnFieldNode([]*parse.Node{
+					parse.NewIntNode(0),
+				}),
+			}),
+		),
+	}
+	source, err := Gen(fizzbuzz)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(source)
 }
